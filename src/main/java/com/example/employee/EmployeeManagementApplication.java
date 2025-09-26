034d86e6ed69225410624ad144523038a1ad89b8
sanjay-billa <sanjaybilla1999@gmail.com>
2025-09-26 12:56:08 -0500

    pre code

--- src/main/java/com/example/employee/EmployeeManagementApplication.java
+++ src/main/java/com/example/employee/EmployeeManagementApplication.java
@@ -1,11 +1,23 @@
 package com.example.employee;
 
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(EmployeeRepository repo) {
+        return args -> {
+            repo.save(new Employee("San", "Developer", 75000.0));
+            repo.save(new Employee("Jay", "Devops", 65000.0));
+        };
     }
 }
